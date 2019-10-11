c0128823c15a7b4c9b5d18c61e7419f80bc88b8d
Rodrigo Goncales <goncales@capintel.ca>
2019-10-10 23:03:30 -0400

    start of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './themes-dist-5.1-gcweb/css/theme.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav class="gcweb-menu wb-init gcweb-menu-inited" typeof="SiteNavigationElement" id="wb-auto-2">
+        <div className="container">
+          <h2 class="wb-inv">Menu</h2>
+          <button
+            type="button"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <span class="wb-inv">
+              Main
+            </span>
+            Menu
+            <span class="expicon glyphicon glyphicon-chevron-down"></span>
+          </button>
+        </div>
+      </nav>
     </div>
   );
 }
